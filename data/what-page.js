72a027fa4e348679de84b56692366bd1d02c06a9
Callum Flack <callum@callumflack.design>
2021-07-02 16:11:25 +1000

    What page: update section name paths with redirects

--- data/what-page.js
+++ data/what-page.js
@@ -2,7 +2,7 @@ export default {
   blocks: [
     {
       label: "Our Services",
-      to: "/what-we-do/corporate-unit",
+      to: "/what-we-do/our-services",
       image:
         "https://www.datocms-assets.com/11614/1557298392-block-bw-fierce.jpg?w=500&q=40&auto=format",
       color: "bg-brand-red",
@@ -10,7 +10,7 @@ export default {
     {
       label: "Our Projects",
       sublabel: "Program & Client Services",
-      to: "/what-we-do/pacs-unit",
+      to: "/what-we-do/our-projects",
       image:
         "https://www.datocms-assets.com/11614/1557789777-block-bw-stare.jpg?w=500&q=40&auto=format",
       color: "bg-brand-green",
@@ -18,15 +18,15 @@ export default {
     {
       label: "Resources",
       sublabel: "Community Awareness, Resources & Support",
-      to: "/what-we-do/cars-unit",
+      to: "/what-we-do/resources",
       image:
         "https://www.datocms-assets.com/11614/1557789791-block-bw-troupe.jpg?w=500&q=40&auto=format",
       color: "bg-brand-blue",
     },
     {
-      label: "Compliments & Complaints",
+      label: "Client Service Chart",
       sublabel: "Financial Logistics and Internal Controls Unit",
-      to: "/what-we-do/flaic-unit",
+      to: "/what-we-do/client-service-chart",
       image:
         "https://www.datocms-assets.com/11614/1559702407-block-bw-flaic.jpg?w=500&q=40&auto=format",
       color: "bg-brand-orange",
